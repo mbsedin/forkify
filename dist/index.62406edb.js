29f5ec0651b08e4c7f3378c9053a1e46cd0bf4c8
Massamba-Bienvenu <boukamass@gmail.com>
2022-02-20 04:40:58 +0100

    loading a single recipe from the api

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -1,11 +1,13 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 const timeout = function(s) {
     return new Promise(function(_, reject) {
         setTimeout(function() {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+fetch(`https://forkify-api.herokuapp.com/api/get?rId=47746`);
 
 //# sourceMappingURL=index.62406edb.js.map
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,6 @@
-const recipeContainer = document.querySelector('.recipe');
+const { async } = require("regenerator-runtime");
+
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +13,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/get?rId=47161`
+    );
+    const data = await res.json();
+    // Send customer error message to the tach() method
+    if (!res.ok) throw new Error(`${data.error} (${res.status})`);
+
+    // replace dashed_names properties from the api
+    let { recipe } = data; // object destructuring , fields should match
+    recipe = {
+      publisher: recipe.publisher,
+      ingredients: recipe.ingredients,
+      sourceUrl: recipe.source_url,
+      recipeId: recipe.recipe_id,
+      imageUrl: recipe.image_url,
+      socialRank: recipe.social_rank,
+      publisherUrl: recipe.publisher_url,
+      title: recipe.title,
+    };
+    console.log(recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+showRecipe();
